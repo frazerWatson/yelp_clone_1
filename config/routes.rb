4013328541496369fd8359a3bac24a29e942f500
Frazer Watson <frazerw@tutanota.com>
2016-03-04 17:48:42 +0000

    Implementing Devise gem.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get 'restaurants' => 'restaurants#index'
   
   resources :restaurants do
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+    root 'restaurants#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
